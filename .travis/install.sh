035396109d697c8f955606e4e6fc884dc46c64d2
Jan Kotanski <jankotan@gmail.com>
2018-11-20 23:57:47 +0100

    add quota

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 # workaround for incomatibility of default ubuntu 16.04 and tango configuration
-if [ $1 = "ubuntu16.04" ]; then
+if [ "$1" = "ubuntu16.04" ]; then
     docker exec -it --user root s2i sed -i "s/\[mysqld\]/\[mysqld\]\nsql_mode = NO_ZERO_IN_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION/g" /etc/mysql/mysql.conf.d/mysqld.cnf
 fi
 
 echo "restart mysql"
-if [ $1 = "debian9" ]; then
+if [ "$1" = "debian9" ]; then
     # workaround for a bug in debian9, i.e. starting mysql hangs
     docker exec -it --user root s2i service mysql stop
     docker exec -it --user root s2i /bin/sh -c '$(service mysql start &) && sleep 30'
@@ -15,13 +15,13 @@ else
 fi
 
 docker exec -it --user root s2i /bin/sh -c 'export DEBIAN_FRONTEND=noninteractive; apt-get -qq update; apt-get -qq install -y tango-db tango-common; sleep 10'
-if [ $? -ne "0" ]
+if [ "$?" -ne "0" ]
 then
     exit -1
 fi
 echo "install tango servers"
 docker exec -it --user root s2i /bin/sh -c 'export DEBIAN_FRONTEND=noninteractive;  apt-get -qq update; apt-get -qq install -y tango-starter tango-test liblog4j1.2-java'
-if [ $? -ne "0" ]
+if [ "$?" -ne "0" ]
 then
     exit -1
 fi
@@ -29,26 +29,26 @@ fi
 docker exec -it --user root s2i service tango-db restart
 docker exec -it --user root s2i service tango-starter restart
 
-if [ $2 = "2" ]; then
+if [ "$2" = "2" ]; then
     echo "install python-pytango"
     docker exec -it --user root s2i /bin/sh -c 'export DEBIAN_FRONTEND=noninteractive; apt-get -qq update; apt-get -qq install -y   python-pytango python-tz python-setuptools python-sphinx'
 else
     echo "install python3-pytango"
     docker exec -it --user root s2i /bin/sh -c 'export DEBIAN_FRONTEND=noninteractive; apt-get -qq update; apt-get -qq install -y   python3-pytango python3-tz python3-setuptools python3-sphinx'
 fi
-if [ $? -ne "0" ]
+if [ "$?" -ne "0" ]
 then
     exit -1
 fi
 
-if [ $2 = "2" ]; then
+if [ "$2" = "2" ]; then
     echo "install PyBenchmarkTarget"
     docker exec -it --user root s2i /bin/sh -c 'cd ds/PyBenchmarkTarget; python setup.py -q install'
 else
     echo "install PyBenchmarkTarget"
     docker exec -it --user root s2i /bin/sh -c 'cd ds/PyBenchmarkTarget; python3 setup.py -q install'
 fi
-if [ $? -ne "0" ]
+if [ "$?" -ne "0" ]
 then
     exit -1
 fi
