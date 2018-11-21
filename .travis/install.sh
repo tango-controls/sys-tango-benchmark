54825d7f4198f714e9fd8a8eef87f8c2942d9947
Jan Kotanski <jankotan@gmail.com>
2018-11-21 20:20:20 +0100

    add buster tests

--- .travis/install.sh
+++ .travis/install.sh
@@ -5,14 +5,8 @@ if [ "$1" = "ubuntu16.04" ]; then
     docker exec -it --user root s2i sed -i "s/\[mysqld\]/\[mysqld\]\nsql_mode = NO_ZERO_IN_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION/g" /etc/mysql/mysql.conf.d/mysqld.cnf
 fi
 
-echo "restart mysql"
-if [ "$1" = "debian9" ]; then
-    # workaround for a bug in debian9, i.e. starting mysql hangs
-    docker exec -it --user root s2i service mysql stop
-    docker exec -it --user root s2i /bin/sh -c '$(service mysql start &) && sleep 30'
-else
-    docker exec -it --user root s2i service mysql restart
-fi
+docker exec -it --user root s2i service mysql stop
+docker exec -it --user root s2i /bin/sh -c '$(service mysql start &) && sleep 30'
 
 docker exec -it --user root s2i /bin/sh -c 'export DEBIAN_FRONTEND=noninteractive; apt-get -qq update; apt-get -qq install -y tango-db tango-common; sleep 10'
 if [ "$?" -ne "0" ]
