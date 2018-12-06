be10be2e4a3e66d6e9d355d76c6777cf3cba1021
Jan Kotanski <jankotan@gmail.com>
2018-12-06 23:38:56 +0100

    clean the code

--- ds/PyBenchmarkTarget/test/PyBenchmarkTarget_test.py
+++ ds/PyBenchmarkTarget/test/PyBenchmarkTarget_test.py
@@ -290,9 +290,10 @@ class PyBenchmarkTargetDeviceTest(unittest.TestCase):
         for i in range(10):
             self.proxy.BenchmarkPipe = wvl
             self.assertEqual(self.proxy.PipeWritesCount, i + 1)
-            rvl = self.proxy.BenchmarkPipe
+            # rvl =
+            self.proxy.BenchmarkPipe
             self.assertEqual(self.proxy.PipeReadsCount, i + 1)
-            #: can be uncommented when pipe implemention required 
+            #: can be uncommented when pipe implemention required
             # self.assertEqual(wvl[0], rvl[0])
             # self.assertEqual(len(wvl[0]), len(rvl[0]))
             # for i in range(len(wvl)):
