38b44936b8508bfd6021fec583a1ea65c718f5e2
Jan Kotanski <jankotan@gmail.com>
2018-12-11 23:57:39 +0100

    update pom.xml

--- .travis/install.sh
+++ .travis/install.sh
@@ -38,6 +38,11 @@ fi
 echo "install CppBenchmarkTarget"
 docker exec -it --user root s2i /bin/sh -c 'curl -O https://people.debian.org/~picca/libtango-java_9.2.5a-1_all.deb; dpkg -i ./libtango-java_9.2.5a-1_all.deb'
 docker exec -it --user root s2i /bin/sh -c 'cd ds/CppBenchmarkTarget; make'
+if [ "$?" -ne "0" ]
+then
+    exit -1
+fi
+echo "install CppBenchmarkTarget"
 docker exec -it --user root s2i /bin/sh -c 'cd ds/JavaBenchmarkTarget; mvn clean install'
 if [ "$?" -ne "0" ]
 then
