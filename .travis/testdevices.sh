a629ef978503cade31490b1c5668e9446d67b38f
Jan Kotanski <jankotan@gmail.com>
2019-02-01 00:16:21 +0100

    slit testsuits for runner and devices

--- .travis/testdevices.sh
+++ .travis/testdevices.sh
@@ -10,14 +10,3 @@ if [ "$?" -ne "0" ]
 then
     exit -1
 fi
-
-echo "run benchmark runner tests"
-if [ "$1" = "2" ]; then
-    docker exec -it --user root s2i /bin/sh -c 'cd benchmarks; python setup.py test'
-else
-    docker exec -it --user root s2i /bin/sh -c 'cd benchmarks; python3 setup.py test'
-fi
-if [ "$?" -ne "0" ]
-then
-    exit -1
-fi
