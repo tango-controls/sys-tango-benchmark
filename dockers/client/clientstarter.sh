535a691aa351db2dfef99b39ba8e9d0396f4d149
Jan Kotanski <jankotan@gmail.com>
2019-03-27 23:23:57 +0100

    add tango host info

--- dockers/client/clientstarter.sh
+++ dockers/client/clientstarter.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
+echo ""
+echo "TANGO_HOST=$TANGO_HOST"
+echo ""
 if [ $# -eq 0 ]
    then
-       python /opt/waitfordevice.py sys/benchmark/pytarget01 sys/benchmark/cpptarget01
+       python /opt/waitfordevice.py sys/benchmark/pytarget01 sys/benchmark/cpptarget01 sys/benchmark/javatarget01
 else
        python /opt/waitfordevice.py sys/tg_test/1
 fi
