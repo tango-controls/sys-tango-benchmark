4a47e2cc1097315eedacfd525918af9b9e90724b
Jan Kotanski <jankotan@gmail.com>
2019-03-28 07:51:10 +0100

    increase timeout

--- dockers/client/waitfordevice.py
+++ dockers/client/waitfordevice.py
@@ -9,7 +9,9 @@ def main():
         description="waits till the device is running")
     parser.add_argument('devices', metavar='N', type=str, nargs='*',
                         help='a list of devices to wait for')
-
+    parser.add_argument(
+        '-t', '--timeout', dest='timeout', type=int, default=1000,
+        help='timeout in seconds (default: 1000 s)')
     args = parser.parse_args()
     if not args.devices:
         args.devices = ["sys/tg_test/1"]
@@ -18,7 +20,7 @@ def main():
 
     for device in args.devices:
         print("Waiting for: %s" % device)
-        status = st.checkDevice(device, 300)
+        status = st.checkDevice(device, args.timeout)
         if not status:
             print("%s is not running" % device)
 
